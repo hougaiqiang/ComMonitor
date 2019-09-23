c983de9452e31dd7be6448db4472e3bedcf006d2
侯改强 <hougaiqiaing@qq.com>
2019-09-23 17:03:45 +0800

    界面设计更新

--- ComMonitor/mainwindow.cpp
+++ ComMonitor/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,25 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/*****************************
+*File->Open点击动作
+*
+*
+*****************************/
+void MainWindow::on_actionOpen_triggered()
+{
+   QString curPath=QDir::currentPath();//获取系统当前目录
+   QString dlgTitle="打开日志"; //对话框标题
+   QString filter="日志文件(*.log);;文本文件(*.txt);;所有文件(*.*)"; //文件过滤器
+   QString aFileName=QFileDialog::getOpenFileName(this,dlgTitle,curPath,filter);
+   if (aFileName.isEmpty())
+       return;
+   QFile aFile(aFileName);
+       if (!aFile.exists()) //文件不存在
+           return ;
+       if (!aFile.open(QIODevice::ReadWrite | QIODevice::Text))
+           return ;
+       ui->textBrowser->setPlainText(aFile.readAll());  //textEditDevice->setPlainText(aFile.readAll());
 }
